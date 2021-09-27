be301a06a70dca22b49368ed40d586e9e39938a1
Anup Jacob <L00163455@student.lyit.ie>
2021-09-27 09:59:26 +0100

    First week 2

---
+++ loop_demo/__init__.py
@@ -0,0 +1,10 @@
+# ---------------------------------
+
+# File          : 
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  :  
+# Modified Date : 
+# Description   :
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
