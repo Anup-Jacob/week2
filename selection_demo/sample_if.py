b8aa733fc043c10475f882295f06180148d70cd0
Anup Jacob <L00163455@student.lyit.ie>
2021-09-27 10:08:06 +0100

    If sample program week 2

---
+++ selection_demo/sample_if.py
@@ -0,0 +1,10 @@
+# ---------------------------------
+
+# File          : 
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  :  
+# Modified Date : 
+# Description   : If example program
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
