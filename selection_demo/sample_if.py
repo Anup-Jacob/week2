5484ec7c15dfbecac48ec767c25b11d6333579be
Anup Jacob <L00163455@student.lyit.ie>
2021-09-27 10:16:36 +0100

    If sample program week 2

--- selection_demo/sample_if.py
+++ selection_demo/sample_if.py
@@ -8,3 +8,10 @@
 # Description   : If example program
 # Licensing     : Anup Jacob, LYIT
 # ----------------------------------
+
+if __name__ == '__main__':
+    grade = 50
+    # print(grade)
+
+    if(grade >= 40):
+        print('You are passed!')
