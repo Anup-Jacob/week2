998b8068a68e85793075ef424feaed9fa612e260
Anup Jacob <L00163455@student.lyit.ie>
2021-09-27 11:24:33 +0100

    First week 2

---
+++ loop_demo/for_example.py
@@ -0,0 +1,19 @@
+# ---------------------------------
+
+# File          : 
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  :  
+# Modified Date : 
+# Description   :
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
+if __name__ == '__main__':
+    for i in range(10,5,-1):
+        print(i)
+
+print(" ")
+
+if __name__ == '__main__':
+    for i in range(0,15,3):
+        print(i)
---
+++ loop_demo/iterate_letter.py
@@ -0,0 +1,13 @@
+# ---------------------------------
+
+# File          : 
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  :  
+# Modified Date : 
+# Description   :
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
+
+if __name__ == '__main__':
+from string import ascii_lowercase:
