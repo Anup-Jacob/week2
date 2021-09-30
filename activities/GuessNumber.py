854b6736ba00f7c98fc160172a91c101572bf731
Anup Jacob <L00163455@student.lyit.ie>
2021-09-30 09:25:28 +0100

    New changes to guessing game

--- activities/GuessNumber.py
+++ activities/GuessNumber.py
@@ -12,17 +12,29 @@
 
 # print("Enter any number between 1 to 20")
 
-x = int(input("Enter any number between 1 to 20 : "))
+y = int(input("Do you wish to play the game : (0/1) : "))
 
-NumValue = 15
+while y != 0:
 
-if (x == 15) :
-    print("Your guess was correct!. Congratulations")
-elif (x > 20):
-    print("The number you have chosen is beyond the limit specified.")
-elif (x < 1):
-    print("The number you have chosen is below the limit specified.")
-else:
-    print("Your guess was wrong!. Please try again.")
+    x = int(input("Enter any number between 1 to 20 : "))
+
+    NumValue = 15
+
+    if (x == 15) :
+        print("Your guess was correct!. Congratulations")
+    elif (x > 20):
+        print("The number you have chosen is beyond the limit specified.")
+    elif (x < 1):
+        print("The number you have chosen is below the limit specified.")
+    else:
+        print("Your guess was wrong!. Please try again.")
+
+    y = input("Do you wish to play again: (0/1) : ")
+
+
+
+print(y)
+
+
 
 print("\n"+"Thank you for playing the Guessing game")
